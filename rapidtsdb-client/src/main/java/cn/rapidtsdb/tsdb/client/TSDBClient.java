8cbad5fb97628e70ccbdb2ca22c15e0882ef2b4a
zhou-jered <cyzjylw@126.com>
2021-07-12 16:56:51 +0800

    choas dev

--- rapidtsdb-client/src/main/java/cn/rapidtsdb/tsdb/client/TSDBClient.java
+++ rapidtsdb-client/src/main/java/cn/rapidtsdb/tsdb/client/TSDBClient.java
@@ -7,7 +7,14 @@ public interface TSDBClient {
 
     void writeMetric(String metric, Datapoint dp);
 
-    void writeMetrics(String metric, double value);
+    void writeMetric(String metric, double value);
 
     void writeMetrics(String metric, List<Datapoint> dps);
+
+    List<Datapoint> readMetrics(String metric, long startTimestamp, long endTimestamp);
+
+    List<Datapoint> readMetrics(String metric, long startTimestamp, long endTimestamp, String downsampler);
+
+    List<Datapoint> readMetricsWithAggregation(long startTimestamp, long endTimestamp, String downsampler, String... metrics);
 }
+
